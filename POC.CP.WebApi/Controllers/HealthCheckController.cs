8723b811d01825bec20bd3d689b559f94ef5707f
Jacob Garner <Jacob.Garner@coxautoinc.com>
2019-08-23 10:51:06 -0600

    Add HealthCheck.

---
+++ POC.CP.WebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace POC.CP.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        // GET: api/HealthCheck
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(new {result = "OK", DateTime = DateTime.Now.ToString(CultureInfo.CurrentCulture)});
+        }
+    }
+}
